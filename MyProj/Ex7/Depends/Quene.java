9e616c361991e820f38cfb99facad9c6ea7434a2
Chongbing Yu <bd8ejk@foxmail.com>
2025-03-17 12:35:37 +0800

    fix: fix the issues of `ex7_2`
    Confused the concepts of stack and queue.
    
    Signed-off-by: Chongbing Yu <bd8ejk@foxmail.com>

--- MyProj/Ex7/Depends/Quene.java
+++ MyProj/Ex7/Depends/Quene.java
@@ -6,12 +6,12 @@ import java.lang.RuntimeException;
 
 public class Quene <T> {
     private int front = 0;
-    private int real = 0;
+    private int rear = 0;
     private ArrayList<T> list = new ArrayList<>();
 
     /// 入队
     public void inQuene(T t){
-        real++;
+        rear++;
         list.add(t);
         System.out.println("队列加入元素：" + t);
     }
@@ -21,20 +21,22 @@ public class Quene <T> {
             throw new RuntimeException("队列为空");
         }
         front++;
-        T t = list.getLast();
-        list.removeLast();
+        T t = list.getFirst();
+        list.removeFirst();
         System.out.println("队列弹出元素：" + t);
         return t;
     }
     /// 判断是否为空
     public boolean isEmpty(){
-        return real == front;
+        return rear == front;
     }
     /// 显示队列
     public void showQuene(){
         System.out.println("队列显示元素：");
+        System.out.println("| " + "Front" + " |");
         for (T t : list) {
             System.out.println("| " + t + " |");
         }
+        System.out.println("| " + "Rear" + " |");
     }
 }
